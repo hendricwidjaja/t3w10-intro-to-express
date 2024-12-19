b88e12b05323c238c33e541066c22129fc4dbd1f
hendricwidjaja <hcwidjaja@gmail.com>
2024-12-19 19:35:23 +1100

    feat: Parameters fetched from the URL

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 
-const { router } = require("../routes/users");
+const router = require("../routes/users");
 
 // Instance of the express for configuration
 const app = express();
